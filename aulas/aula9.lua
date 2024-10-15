33084e6e7e14eda6e083c2f588e810f698a67db3
Vitorcostalv <vitorcosta99900@gamil.com>
2024-10-15 01:56:25 -0300

    testes

---
+++ aulas/aula9.lua
@@ -0,0 +1,4 @@
+local texto = "Curso de Lua"
+print(string.upper(texto))  -- Converte para maiúsculas
+print(string.len(texto))     -- Tamanho da string
+print(string.sub(texto, 1, 5))  -- Substring
