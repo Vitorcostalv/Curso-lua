38a72e312d7dfa88d08c7382b32a97dc5db10cee
Vitorcostalv <vitorcosta99900@gamil.com>
2024-10-15 01:57:46 -0300

    Arquivo de configuração em lua

---
+++ aulas/aula11.lua
@@ -0,0 +1,9 @@
+local config = {
+    volume = 75,
+    resolucao = {largura = 1920, altura = 1080},
+    fullscreen = true
+}
+
+print("Volume:", config.volume)
+print("Resolução:", config.resolucao.largura .. "x" .. config.resolucao.altura)
+print("Fullscreen:", config.fullscreen)
